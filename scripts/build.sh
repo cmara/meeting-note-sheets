b61e48823717bf395b013b41b6ac1b20e4b7b5df
Mark Ide <git@cranstonide.com>
2017-03-15 23:26:13 -0400

    Correct file paths

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,18 +4,18 @@ mkdir -p output/
 
 # Board Meeting Notes
 # Takes two passes to get page number references correct.
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory output board-meeting-note-sheet.tex
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory output board-meeting-note-sheet.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory output documents/board-meeting-note-sheet.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory output documents/board-meeting-note-sheet.tex
 
 # Business Meeting Notes
 # Takes two passes to get page number references correct.
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory output business-meeting-note-sheet.tex
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory output business-meeting-note-sheet.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory output documents/business-meeting-note-sheet.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory output documents/business-meeting-note-sheet.tex
 
 # Attendance Sheet
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory output attendance-sheet.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory output documents/attendance-sheet.tex
 
 # Meeting Format Reminder Sheet
 # Takes two passes to get page number references correct.
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory output meeting-format-reminder-sheet.tex
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory output meeting-format-reminder-sheet.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory output documents/meeting-format-reminder-sheet.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory output documents/meeting-format-reminder-sheet.tex
